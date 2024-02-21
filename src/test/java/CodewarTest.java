761fa0957239bd18f0dcdaa25f8ee126962f2caa
Bareisha Darya <dariaboreysha@gmail.com>
2024-02-21 13:23:49 +0300

    -Selenide changes

--- src/test/java/CodewarTest.java
+++ src/test/java/CodewarTest.java
@@ -1,26 +1,11 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
-import com.codeborne.selenide.Selenide;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CodewarTest {
 
-    private static void setup() {
-        WebDriverManager.chromedriver().setup();
-    }
 
-    @BeforeEach
-    public void init() {
-        setup();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        Selenide.closeWebDriver();
-    }
 
     @Test
     public void check123Test() {
